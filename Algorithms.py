477e124a421c94e2afcfe03eddeaf34ce38213a7
Vytautas Astrauskas <vastrauskas@gmail.com>
2010-08-13 17:41:48 +0300

    Updated Town extraction algorithm

--- Algorithms.py
+++ Algorithms.py
@@ -40,19 +40,37 @@ class School(object):
                 u' pagr.':              u' pagrindinė',
                 u' vid.':               u' vidurinė',
                 u'Gimnazija':           u'gimnazija',
+                u' G ':                 u' gimnazija ',
+                u'g-ja':                u'gimnazija ',
                 u'm-kla':               u'mokykla',
+                u'm - kla':             u'mokykla',
+                u'm-la':                u'mokykla',
+                u'mok.':                u'mokykla',
                 u'KTUG':         
                         u'Kauno technologijos universiteto gimnazija',
                 u'KTU':         
                         u'Kauno technologijos universiteto ',
+                u'VDU':         
+                        u' Vytauto Didžiojo universiteto ',
+                u'NMKČMM':
+                        u'Nacionalinė Mikalojaus Konstantino '
+                        u'Čiurlionio menų mokykla',
+                }
+        ends = {
+                u'G':                   u' gimnazija ',
                 }
         for k, v in change.items():
             title = title.replace(k, v)
+        for k, v in ends.items():
+            if title.endswith(k):
+                title = title[:-len(k)] + v
 
         if u'Liubertien' in title:      # FIXME: Workaround mismach
             title = u'Viešoji įstaiga Vilniaus privati gimnazija'
         elif u'Kalniečių vidurinė' in title:
             title = u'Kauno Antano Smetonos vidurinė mokykla'
+        if u'ktug' == title:
+            title = u'Kauno technologijos universiteto gimnazija'
 
         self.title = title
 
@@ -65,6 +83,7 @@ class School(object):
                 u'„',
                 u'“',
                 u'-',
+                u'–',
                 u',',
                 u'.',
                 #u'gimnazija',
@@ -122,35 +141,92 @@ class Address(object):
         text = self.text
 
         change = {
-                u' raj.':               u' rajonas',
-                u' r.':                 u' rajonas',
-                u' r.sav':              u' rajonas',
-                u'rajonas.':            u'rajonas',
+                u' raj.':               u' rajonas ',
+                u' raj ':               u' rajonas ',
+                u' r.':                 u' rajonas ',
+                u' r.sav':              u' rajonas ',
+                u'rajonas.':            u' rajonas ',
+                u' km.':                u' kaimas ',
+                u' k.':                 u' kaimas ',
+                u' km,':                u' kaimas, ',
+                u' sav.':               u' savivaldybė ',
+                u' mstl.':              u' miestelis ',
                 u',':                   u' , ',
+                u'.':                   u'. ',
+                u';':                   u' , ',
+                u'LT - ':               u'LT-',
+                u'LT ':                 u'LT-',
                 }
         
         for k, v in change.items():
             text = text.replace(k, v)
 
+        if text.endswith(' raj'):
+            text = text[:-3] + ' rajonas '
+        elif text.endswith(' r'):
+            text = text[:-1] + ' rajonas '
+
         self.town = None
         parts = text.split()
+
+        for part in parts:
+            if (part.isalpha() and part.isupper() and len(part) > 1) and \
+                    (part not in (u'SB',)):
+                raise ConversionError(u'Nepavyko atpažinti miesto! Did.')
+
         for i, part in enumerate(parts):
-            if part == u'rajonas':
-                self.town = u' '.join(parts[i-1:i+1])
+            if part == u'kaimas':
+                if len(parts[i-1]) == 1:
+                    self.town = u' '.join(parts[i-2:i+1])
+                else:
+                    self.town = u' '.join(parts[i-1:i+1])
                 break
+        if not self.town:
+            for i, part in enumerate(parts):
+                if part == u'miestelis':
+                    self.town = u' '.join(parts[i-1:i+1])
+                    break
+        if not self.town:
+            for i, part in enumerate(parts):
+                if part == u'rajonas':
+                    self.town = u' '.join(parts[i-1:i+1])
+                    break
+        if not self.town:
+            for i, part in enumerate(parts):
+                if part == u'savivaldybė':
+                    self.town = u' '.join(parts[i-1:i+1])
+                    break
 
         if not self.town:
+            def notTown(part):
+                if part in (u'g.', u'pr.', u'sen.', u'skg.', u'mstl.') or \
+                        part.endswith(u'ų') or part.endswith(u'u') or \
+                        part.endswith(u'io') or part.endswith(u'ios') or \
+                        part.endswith(u'o') or part.endswith(u'ios') or \
+                        part.endswith(u'ies') or \
+                        part.endswith(u'ės'):
+                    return True
+                return False
+
             for part in reversed(parts):
-                if part in (u'g.', u'pr.', u'km.', u'sen.', u'skg.', 
-                        u'mstl.'):
+                if notTown(part):
+                    if parts[0].isalpha() and parts[0].istitle() and \
+                            not notTown(parts[0]):
+                        self.town = parts[0]
                     break
-                if part[-1] == u'.':
-                    part = part[:-1]
                 if part.isalpha():
+                    if part.endswith(u'e'):
+                        raise ConversionError(u'Nepavyko atpažinti miesto!')
+
                     if part.istitle():
                         self.town = part
                     break
             if not self.town:
                 raise ConversionError(u'Nepavyko atpažinti miesto!')
 
-        self.value = text.replace(u' , ', u',')
+        if self.town == u'A':
+            raise Exception(u'STOP!')
+
+        for i in range(4):
+            text = text.replace(u'  ', u' ').replace(u' ,', u',')
+        self.value = text
